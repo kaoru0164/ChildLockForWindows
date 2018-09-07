9c7d253ef1b20c22a5bf921c5419738daec06bc4
kaoru0164 <kaoru0164@tf6.so-net.ne.jp>
2018-09-07 22:33:22 +0900

    Add KeyHook function.

--- ChildLock/ChildLock/FormMain.cs
+++ ChildLock/ChildLock/FormMain.cs
@@ -1,8 +1,37 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
 
 namespace ChildLock
 {
     class FormMain : Form
     {
+        /// <summary>
+        /// キーボードフックの制御.
+        /// </summary>
+        private KeyboardHook keybordHook = new KeyboardHook();
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            keybordHook.OnKeyPress += KeybordHook_OnKeyPress;
+            keybordHook.Start();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+
+            keybordHook.Stop();
+        }
+
+        /// <summary>
+        /// キーが押された時のイベント.
+        /// </summary>
+        /// <param name="key">押されたキー</param>
+        private void KeybordHook_OnKeyPress(Keys key)
+        {
+            Text = key.ToString();
+        }
     }
 }
