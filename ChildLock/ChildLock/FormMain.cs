c6a70f5ea50e1772c950f6927932ab329a20d7ef
kaoru0164 <kaoru0164@tf6.so-net.ne.jp>
2018-09-08 22:23:43 +0900

    Implemented overlap form.

--- ChildLock/ChildLock/FormMain.cs
+++ ChildLock/ChildLock/FormMain.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace ChildLock
 {
@@ -11,18 +12,79 @@ namespace ChildLock
         /// </summary>
         private KeyboardHook keybordHook = new KeyboardHook();
 
+        /// <summary>
+        /// 背景画像.
+        /// </summary>
+        private Bitmap backgroundBitmap;
+        /// <summary>
+        /// 背景画像の表示位置.
+        /// </summary>
+        private Point backgroundPoint;
+
+        /// <summary>
+        /// キー画像.
+        /// </summary>
+        private Bitmap keyBitmap;
+        /// <summary>
+        /// キー画像の表示位置.
+        /// </summary>
+        private Point keyPoint;
+
+        /// <summary>
+        /// コンストラクタ.
+        /// </summary>
+        public FormMain()
+        {
+            FormBorderStyle = FormBorderStyle.None;
+            StartPosition = FormStartPosition.Manual;
+            SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint | ControlStyles.OptimizedDoubleBuffer
+                , true);
+            WindowState = FormWindowState.Maximized;
+        }
+
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
             keybordHook.OnKeyPress += KeybordHook_OnKeyPress;
             keybordHook.Start();
+
+            Rectangle screenRectangle = Screen.PrimaryScreen.Bounds;
+
+            backgroundBitmap = new Bitmap(screenRectangle.Width, screenRectangle.Height);
+            Graphics graphics = Graphics.FromImage(backgroundBitmap);
+            graphics.CopyFromScreen(new Point(0, 0), new Point(0, 0), backgroundBitmap.Size);
+            graphics.Dispose();
+            backgroundPoint = new Point(0, 0);
+
+            keyBitmap = Properties.Resources.KeyImage;
+            keyPoint = new Point(screenRectangle.Width - keyBitmap.Width - 10, 10);
         }
 
-        protected override void OnClosing(CancelEventArgs e)
+        protected override void OnFormClosing(FormClosingEventArgs e)
         {
-            base.OnClosing(e);
-
+            base.OnFormClosing(e);
             keybordHook.Stop();
+        }
+
+        protected override void OnMouseClick(MouseEventArgs e)
+        {
+            if (keyPoint.X <= e.X
+                && e.X <= keyPoint.X + keyBitmap.Width
+                && keyPoint.Y <= e.Y
+                && e.Y <= keyPoint.Y + keyBitmap.Height)
+            {
+                Close();
+            }
+
+            base.OnMouseClick(e);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            e.Graphics.DrawImage(backgroundBitmap, backgroundPoint);
+            e.Graphics.DrawImage(keyBitmap, keyPoint);
         }
 
         /// <summary>
